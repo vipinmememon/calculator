e05d16d98153ea31f3819e59074e0c9a518c24c8
Er. Vipin kumar <166358641+vipinmememon@users.noreply.github.com>
2024-07-18 21:55:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let input=document.querySelector("#inputbox");
+let buttons=document.querySelectorAll("button");
+let string="";
+let arr=Array.from(buttons);
+arr.forEach(buttons=>{
+    buttons.addEventListener("click",(e)=>{
+        if(e.target.innerHTML=='='){
+            string=eval(string);  //eval is a inbuilt function that calculate the operation
+            input.value=string;
+        }else if(e.target.innerHTML=='AC'){
+            string="";
+            input.value=string;
+        }else if(e.target.innerHTML=='DEL'){
+string=string.substring(0,string.length-1);
+input.value=string;
+        }
+        else{
+            string+=e.target.innerHTML;
+        input.value=string;
+        }
+        
+    })
+})
